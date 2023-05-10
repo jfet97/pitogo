e5558d89b76f5a0444a928dd7f323d58db7816c2
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-05-10 21:19:03 +0200

    transpile log

--- src/transpiler/transpiler.test.ts
+++ src/transpiler/transpiler.test.ts
@@ -1,11 +1,18 @@
-import * as S from "../scanner/index.js"
-import * as P from "../parser/index.js"
-import { transpileToGo } from "./transpiler.js"
+import * as S from '../scanner/index.js';
+import * as P from '../parser/index.js';
+import { transpileToGo } from './transpiler.js';
 
-const ast = P.parse(S.scanner(`
+const ast = P.parse(
+  S.scanner(`
 A = (a)a<a>;
 B = (b)b<b>;
 C(x) = x<"yo">;
 main = log<"yo"> | A + B | ((a)C<a>);
-`))
-console.log(transpileToGo(ast))
+`),
+);
+
+const go = transpileToGo(ast);
+console.log(go);
+import { writeFileSync } from 'fs';
+
+writeFileSync('./src/transpiler/program.go', go);
