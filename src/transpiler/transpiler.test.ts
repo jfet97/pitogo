aac99a33ea61adde4754a93eaca00668eac8a0fb
Alessandro Scala <alessandro.scala8@gmail.com>
2023-05-10 19:31:01 +0200

    Implement more transpiler cases

--- src/transpiler/transpiler.test.ts
+++ src/transpiler/transpiler.test.ts
@@ -2,14 +2,10 @@ import * as S from "../scanner/index.js"
 import * as P from "../parser/index.js"
 import { transpileToGo } from "./transpiler.js"
 
-console.log(transpileToGo(P.parse(S.scanner(`
-A = a<a>;
-B = b<b>;
-main = log<"yo">;
-`))))
-
-console.log(P.parse(S.scanner(`
-A = a<a>;
-B = b<b>;
-main = log<"yo">;
-`)))
+const ast = P.parse(S.scanner(`
+A = (a)a<a>;
+B = (b)b<b>;
+C(x) = x<"yo">;
+main = log<"yo"> | A + B | ((a)C<a>);
+`))
+console.log(transpileToGo(ast))
--- src/transpiler/transpiler.ts
+++ src/transpiler/transpiler.ts
@@ -30,15 +30,15 @@ type Message struct {
 	value interface{}
 }
 
-func NewMessageNumber(n int) Message {
+func NewNumberMessage(n int) Message {
 	return Message{value: n}
 }
 
-func NewMessageString(s string) Message {
+func NewStringMessage(s string) Message {
 	return Message{value: s}
 }
 
-func NewMessageChannel() Message {
+func NewChannelMessage() Message {
 	ch := make(chan Message)
 	return Message{value: ch}
 }
@@ -56,11 +56,20 @@ func (m Message) String() string {
 	}
 }
 
+func (m Message) Channel() (chan Message) {
+	switch v := m.value.(type) {
+	case chan Message:
+		return v
+	default:
+		panic("Trying to use a non channel message as channel")
+	}
+}
+
 func (m Message) Println() {
 	fmt.Println(m.String())
 }
 
-// usage: fmt.Println(NewMessageString("hello"))
+// usage: fmt.Println(NewStringMessage("hello"))
 // result: Message<"hello">
 
 ${ast.declarations.map((d) => transpileToGo(d)).join('\n')}
@@ -88,14 +97,55 @@ ${transpileToGo(ast.process)}`
     }
 
     case P.NODES.StringLiteral: {
-      return `"${ast.value}"`
+      return `NewStringMessage("${ast.value}")`
+    }
+
+    case P.NODES.NumberLiteral: {
+      return `NewNumberMessage(${ast.value})`
+    }
+
+    case P.NODES.Identifier: {
+      return ast.identifier
     }
 
     case P.NODES.Declaration: {
-      return "asdasd"
+      return `func ${ast.identifier.identifier}(`+
+        ast.parameters
+          .map((identifier) => identifier.identifier + " Message")
+          .join(", ") + ") {\n" +
+        transpileToGo(ast.process) + "\n}"
     }
 
+    case P.NODES.SendMessage: {
+      return `${ast.channel.identifier}.Channel() <- ${transpileToGo(ast.message)}`
+    }
+
+    case P.NODES.Restriction: {
+      return ast.channels
+        .map((channel) => `${channel.identifier} := NewChannelMessage()`)
+        .join("\n") + "\n" +
+        transpileToGo(ast.process)
+    }
+
+    case P.NODES.ParallelComposition: {
+      return ast.processes
+        .map((proc) => `go func(){
+          ${transpileToGo(proc)}
+        }()`)
+        .join("\n")
+    }
+
+//     case P.NODES.NonDeterministicChoice: {
+//       const phonyProcesses = ast.processes.filter((process) => process._tag !== P.NODES.ActionPrefix)
+
+//       phonyProcesses.map((_, i) => `PhOnY${i} := make(int, 1)
+// PhOnY${i} <- 0`).join("\n")
+//       return `select {
+//         ${ast.processes.map((process) => )}
+//       }`
+//     }
+
     default:
-      return `${ast._tag} Not Implemented`;
+      return `${ast._tag} Is Implemented`;
   }
 }
